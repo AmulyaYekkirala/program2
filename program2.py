3b42cf3c37a329bac279a98f7e2a6f157b9b75fe
ammu <amulyayekkirala@gmail.com>
2024-12-26 14:23:03 +0530

    second commit

---
+++ program2.py
@@ -0,0 +1,6 @@
+#find largest of 3 numbers
+num1=int(input("enter first number"))
+num2=int(input("enter second number"))
+num3=int(input("enter third number"))
+largest=max(num1,num2,num3)
+print(largest)
